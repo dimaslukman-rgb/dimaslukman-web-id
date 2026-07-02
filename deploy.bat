899024af44ccf70a2401e860503ad8e740f7d116
Dimas Lukman <dimaslukman@gmail.com>
2026-07-02 21:50:46 +0700

    Add CNAME and cleanup

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "c:\Users\ASUS\Documents\Agent Router\portofolio3"
-git config user.email "dimaslukman@gmail.com"
-git config user.name "Dimas Lukman"
-git add -A
-git commit -m "Initial commit"
-git remote add origin https://github.com/dimaslukman-rgb/dimaslukman-web-id.git
-git branch -M main
-git push -u origin main
